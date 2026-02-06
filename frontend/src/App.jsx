352e18fcf7ee5e8d7b808b8e4e3c60971ea1b910
Lester Lou Reyes <llreyes.589@gmail.com>
2026-02-06 23:30:17 +0800

    Add auto deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,13 @@ function App() {
   const [count, setCount] = useState(0);
 
   const fetchCount = async () => {
-    const res = await fetch("http://localhost:4000/count");
+    const res = await fetch("http://108.175.8.128:4000/count");
     const data = await res.json();
     setCount(data.count);
   };
 
   const increment = async () => {
-    await fetch("http://localhost:4000/inc", { method: "POST" });
+    await fetch("http://108.175.8.128:4000/inc", { method: "POST" });
     fetchCount();
   };
 
